375890ff9d29ce29f00a103cb5967ef927a48bc3
Gabriel Cavlovic <gabriel.cavlovic77@gmail.com>
2021-05-21 22:14:28 +0200

    added theme.js file

---
+++ src/style/theme.js
@@ -0,0 +1,16 @@
+export const breakpoints = {
+  mobileLarge: 'min-width: 576px',
+  tablet: 'min-width: 768px',
+  desktop: 'min-width: 1024px',
+  desktopLarge: 'min-width: 1300px',
+};
+
+export const colors = {
+  white: '#ffffff',
+  black: '#000000',
+  darkGrey: '#333333',
+  lightGrey: '#cccccc',
+  backgroundGrey: '#f5f5f5',
+  blue: '#15698f',
+  darkBlue: '#145a8c',
+};
