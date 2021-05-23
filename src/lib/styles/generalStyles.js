87f2c79f766cd346cf858a08faabe3af4161309e
Tomislav Fulir <tomislavf5@gmail.com>
2021-05-23 23:55:55 +0200

    Implemented login page with a yup/formik handled form. Added inputs as styled components

--- src/lib/styles/generalStyles.js
+++ src/lib/styles/generalStyles.js
@@ -1,3 +1,54 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { colors, boxShadowFocus } from './theme';
 
+//Main
 export const Main = styled.div``;
+
+//Input components
+export const InputGeneral = css`
+  height: 56px;
+  border-radius: 4px;
+  border: solid 1px ${colors.lightGrey};
+  font-size: 16px;
+  padding: 8px 5px;
+
+  &:focus {
+    border-color: ${colors.blue};
+    outline: none;
+    box-shadow: ${boxShadowFocus};
+  }
+
+  width: ${(props) => (props.width ? props.width : '100%')};
+`;
+
+export const Input = styled.input`
+  ${InputGeneral};
+`;
+
+export const Select = styled.select`
+  ${InputGeneral};
+`;
+
+export const Option = styled.option``;
+
+export const TextArea = styled.textarea`
+  ${InputGeneral};
+  height: 112px;
+`;
+
+export const Label = styled.label`
+  font-size: 16px;
+  margin-bottom: 10px;
+  display: inline-block;
+  background-color: ${colors.white};
+  font-weight: bold;
+  color: ${colors.blue};
+`;
+
+export const ErrorLabel = styled.p`
+  font-size: 14px;
+  display: inline-block;
+  color: ${colors.red};
+  background-color: ${colors.white};
+  padding-top: 8px;
+`;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import headingImage from '../../assets/images/login-heading.png';
+import { Overlay, Figure, Image, Form, FormRow } from './LoginStyle';
+import { Input, Label, ErrorLabel } from '../../lib/styles/generalStyles';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const Login = () => {
-  return <div></div>;
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Invalid email address!')
+        .required('Email is required!'),
+      password: Yup.string()
+        .min(8, 'Password must be at least 8 characters long!')
+        .required('Password is required!'),
+    }),
+    onSubmit: (values) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values));
+      }, 1000);
+    },
+  });
+
+  return (
+    <>
+      <Overlay>
+        <Figure>
+          <Image src={headingImage} />
+        </Figure>
+        <Form onSubmit={formik.handleSubmit}>
+          <FormRow>
+            <Label htmlFor="email">Email:</Label>
+            <Input type="email" id="email" {...formik.getFieldProps('email')} />
+            {formik.touched.email && formik.errors.email ? (
+              <ErrorLabel>{formik.errors.email}</ErrorLabel>
+            ) : null}
+          </FormRow>
+          <FormRow>
+            <Label>Lozinka:</Label>
+            <Input
+              type="password"
+              id="password"
+              {...formik.getFieldProps('password')}
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <ErrorLabel>{formik.errors.password}</ErrorLabel>
+            ) : null}
+          </FormRow>
+          <FormRow>{/* Add submit button */}</FormRow>
+        </Form>
+      </Overlay>
+    </>
+  );
 };
 
 export default Login;
