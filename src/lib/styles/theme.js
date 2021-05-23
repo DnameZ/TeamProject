291e8161128189a6c82f22de4e4109ddc84c84c4
Gabriel Cavlovic <gabriel.cavlovic77@gmail.com>
2021-05-23 23:05:43 +0200

    folder structure, routes, pages, node modules

--- src/lib/styles/theme.js
+++ src/lib/styles/theme.js
@@ -4,7 +4,6 @@ export const breakpoints = {
   desktop: 'min-width: 1024px',
   desktopLarge: 'min-width: 1300px',
 };
-
 export const colors = {
   white: '#ffffff',
   black: '#000000',
@@ -13,4 +12,9 @@ export const colors = {
   backgroundGrey: '#f5f5f5',
   blue: '#15698f',
   darkBlue: '#145a8c',
+  silver: '#bbbbbb',
+  red: '#c2002c',
 };
+export const boxShadow = '1px 1px 4px 0 rgba(0, 0, 0, 0.1)';
+export const boxShadowHover = '2px 2px 8px 0 rgba(0, 0, 0, 0.1)';
+export const boxShadowFocus = '2px 2px 8px 0 rgba(0, 0, 0, 0.1)';
