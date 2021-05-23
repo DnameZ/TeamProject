f5fb1d5450575547252ac8a56fe40c4e1666b244
Gabriel Cavlovic <gabriel.cavlovic77@gmail.com>
2021-05-23 20:41:52 +0200

    created EventCard component

---
+++ src/components/EventCard/EventCard.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { BsChevronExpand, BsChevronContract } from 'react-icons/bs';
+
+// style
+import {
+  EventCard as EventCardWrapper,
+  EventCardHeader,
+  EventCardTitle,
+  EventCardIcon,
+  EventCardContent,
+  ContentRow,
+  ContentRowShow,
+  ContentRowShowDesktop,
+  RowTitle,
+  RowValue,
+  ButtonWrapper,
+} from './EventCardStyle';
+
+const EventCard = ({
+  title,
+  location,
+  date,
+  time,
+  freeSpots,
+  company,
+  shortDescription,
+}) => {
+  const [showDescription, setShowDescrption] = useState(false);
+
+  return (
+    <>
+      <EventCardWrapper>
+        <EventCardHeader>
+          <EventCardTitle>{title}</EventCardTitle>
+          <EventCardIcon>
+            {showDescription ? (
+              <BsChevronContract
+                size={18}
+                color={'#333333'}
+                onClick={() => setShowDescrption(!showDescription)}
+              />
+            ) : (
+              <BsChevronExpand
+                size={18}
+                color={'#333333'}
+                onClick={() => setShowDescrption(!showDescription)}
+              />
+            )}
+          </EventCardIcon>
+        </EventCardHeader>
+
+        <EventCardContent>
+          <ContentRow>
+            <RowTitle>Lokacija</RowTitle>
+            <RowValue>{location}</RowValue>
+          </ContentRow>
+
+          <ContentRow>
+            <RowTitle>Datum i vrijeme</RowTitle>
+            <RowValue>{date}</RowValue>
+            <RowValue>{time}</RowValue>
+          </ContentRow>
+
+          <ContentRow>
+            <RowTitle>Slobodna mjesta</RowTitle>
+            <RowValue>{freeSpots}</RowValue>
+          </ContentRow>
+
+          <ContentRow>
+            <RowTitle>Firma</RowTitle>
+            <RowValue>{company}</RowValue>
+          </ContentRow>
+
+          <ContentRowShowDesktop>
+            <RowTitle>Kratki opis</RowTitle>
+            <RowValue>{shortDescription}</RowValue>
+          </ContentRowShowDesktop>
+
+          {showDescription && (
+            <ContentRowShow>
+              <RowTitle>Kratki opis</RowTitle>
+              <RowValue>{shortDescription}</RowValue>
+            </ContentRowShow>
+          )}
+        </EventCardContent>
+        <ButtonWrapper></ButtonWrapper>
+      </EventCardWrapper>
+    </>
+  );
+};
+
+export default EventCard;
---
+++ src/components/EventCard/EventCardStyle.js
@@ -0,0 +1,133 @@
+import styled from 'styled-components';
+import { colors, breakpoints } from '../../style/theme';
+
+export const EventCard = styled.div`
+  width: 288px;
+  margin: 0 auto;
+  margin-top: 16px;
+  padding: 16px 2px;
+  border-radius: 4px;
+  border: solid 2px ${colors.lightGrey};
+  background-color: ${colors.white};
+
+  &:hover {
+    box-shadow: 2px 2px 8px 0 rgba(0, 0, 0, 0.1);
+    border: solid 2px ${colors.blue};
+  }
+
+  @media screen and (${breakpoints.tablet}) {
+    width: 688px;
+  }
+
+  @media screen and (${breakpoints.desktop}) {
+    width: 832px;
+  }
+`;
+
+export const EventCardHeader = styled.div`
+  width: 100%;
+  height: 70px;
+  border-bottom: solid 2px ${colors.lightGrey};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const EventCardTitle = styled.h2`
+  width: 230px;
+  height: 54px;
+  margin: 0 16px 16px 14px;
+  font-family: Helvetica;
+  font-size: 18px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: normal;
+  color: ${colors.blue};
+
+  @media screen and (${breakpoints.tablet}) {
+    width: 688px;
+    height: 20px;
+    font-size: 24px;
+  }
+`;
+
+export const EventCardIcon = styled.div`
+  margin-right: 10px;
+
+  @media screen and (${breakpoints.desktop}) {
+    display: none;
+  }
+`;
+
+export const EventCardContent = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  margin: 0 auto;
+
+  @media screen and (${breakpoints.tablet}) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+`;
+
+export const ContentRow = styled.div``;
+
+export const ContentRowShow = styled.div`
+  @media screen and (${breakpoints.tablet}) {
+    grid-column-start: 1;
+    grid-column-end: 5;
+    grid-row-start: 2;
+    grid-row-end: 4;
+    margin-right: 14px;
+  }
+
+  @media screen and (${breakpoints.desktop}) {
+    display: none;
+  }
+`;
+
+export const ContentRowShowDesktop = styled.div`
+  display: none;
+  @media screen and (${breakpoints.desktop}) {
+    display: grid;
+    grid-column-start: 1;
+    grid-column-end: 5;
+    grid-row-start: 2;
+    grid-row-end: 4;
+    margin-right: 14px;
+  }
+`;
+
+export const RowTitle = styled.h3`
+  margin: 16px 0 4px 14px;
+  font-family: Helvetica;
+  font-size: 16px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.63;
+  letter-spacing: normal;
+  color: ${colors.darkGrey};
+
+  @media screen and (${breakpoints.tablet}) {
+    font-size: 18px;
+  }
+`;
+
+export const RowValue = styled.p`
+  margin: 4px 0px 4px 14px;
+  font-family: Helvetica;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: normal;
+  color: ${colors.darkGrey};
+`;
+
+export const ButtonWrapper = styled.div`
+  text-align: center;
+`;
