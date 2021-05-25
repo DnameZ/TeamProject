3b600b93720233082164bcc7b935a37aeedee773
Tomislav Fulir <tomislavf5@gmail.com>
2021-05-26 00:38:19 +0200

    Implemented search component

---
+++ src/components/SearchBar/SearchBarStyle.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const SearchIcon = styled.figure`
+  position: absolute;
+  bottom: 14px;
+  left: 20px;
+`;
+
+export const ClearIcon = styled.figure`
+  position: absolute;
+  bottom: 24px;
+  right: 20px;
+  zoom: 0.7;
+  cursor: pointer;
+`;
+
+export const BarWrapper = styled.div`
+  position: relative;
+`;
