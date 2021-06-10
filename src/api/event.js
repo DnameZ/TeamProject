c1b4e5c9b6738a51bd9ef1aecee59ee14e37c0ad
markomrkonjic479@gmail.com <markomrkonjic479@gmail.com>
2021-06-10 16:03:30 +0200

    Added a time and date

--- src/api/event.js
+++ src/api/event.js
@@ -11,8 +11,6 @@ import apiOrigin from './api';
 //   }).then((res) => res.json());
 // };
 
-import apiOrigin from './api';
-
 export const getAllEvents = (authToken) => {
   return fetch(`${apiOrigin}/api/event`, {
     method: 'GET',
--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -32,6 +32,36 @@ const Events = () => {
     return allEvents === true ? PrijaviSe : OdjaviSe;
   };
 
+  const parseDate = (rawDate) => {
+    const options = {
+      weekday: 'long',
+      month: '2-digit',
+      day: 'numeric',
+    };
+
+    const date = new Date(rawDate);
+    const parsedDate = date.toLocaleDateString('hr', options);
+    return parsedDate.charAt(0).toUpperCase() + parsedDate.slice(1);
+  };
+
+  const parseTime = (rawStartTime, rawEndTime) => {
+    const options = {
+      hour: '2-digit',
+      minute: '2-digit',
+    };
+
+    const parsedStartTime = new Date(rawStartTime).toLocaleTimeString(
+      'hr',
+      options,
+    );
+    const parsedEndTime = new Date(rawEndTime).toLocaleTimeString(
+      'hr',
+      options,
+    );
+
+    return parsedStartTime + ' - ' + parsedEndTime + 'h';
+  };
+
   const toggleFilter = () => {
     setFilter((prevFilter) => !prevFilter);
   };
@@ -77,10 +107,10 @@ const Events = () => {
             {events.map((event) => (
               <EventCard
                 key={event.id}
-                title={event.title}
+                title={event.name}
                 location={event.location}
-                date={event.date}
-                time={event.time}
+                date={parseDate(event.startTime)}
+                time={parseTime(event.startTime, event.endTime)}
                 freeSpots={event.seats}
                 company={event.organizer}
                 shortDescription={event.description}
