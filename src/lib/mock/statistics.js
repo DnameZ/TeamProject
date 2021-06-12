c4936068ff181e63839189cb7833847fc7ce927d
Nikola Kristanović <nikola.kristanovic@gmail.com>
2021-06-12 20:04:21 +0200

    added statistics mock

---
+++ src/lib/mock/statistics.js
@@ -0,0 +1,99 @@
+export const events = [
+  {
+    id: 1,
+    title: 'Kreiraj svoju Pokedex aplikaciju uz ReactJS',
+    avgGrade: 4.7,
+  },
+  {
+    id: 2,
+    title: 'Mogu li “ja” postati konzultant?',
+    avgGrade: 4.6,
+  },
+  {
+    id: 3,
+    title: 'Karijera informatičara nakon FOI-ja',
+    avgGrade: 4.7,
+  },
+  {
+    id: 4,
+    title: 'Kreiraj svoju Pokedex aplikaciju uz ReactJS',
+    avgGrade: 4.7,
+  },
+  {
+    id: 5,
+    title: 'Mogu li “ja” postati konzultant?',
+    avgGrade: 4.9,
+  },
+  {
+    id: 6,
+    title: 'Karijera informatičara nakon FOI-ja',
+    avgGrade: 4.8,
+  },
+];
+
+export const users = [
+  {
+    id: 1,
+    firstName: 'Nikola',
+    lastName: 'Marković',
+    email: 'nikolam@gmail.com',
+    participations: 21,
+    isAdmin: true,
+  },
+  {
+    id: 2,
+    firstName: 'Marko',
+    lastName: 'Mrkonjić',
+    email: 'markomrk@gmail.com',
+    participations: 30,
+    isAdmin: false,
+  },
+  {
+    id: 3,
+    firstName: 'Borna',
+    lastName: 'Grilec',
+    email: 'borna.grilec@gmail.com',
+    participations: 31,
+    isAdmin: true,
+  },
+  {
+    id: 4,
+    firstName: 'Gabrijel',
+    lastName: 'Čalović',
+    email: 'gabrijel.calovicm@gmail.com',
+    participations: 25,
+    isAdmin: false,
+  },
+  {
+    id: 5,
+    firstName: 'Tomislav',
+    lastName: 'Fulir',
+    email: 'tomislav.fulir@gmail.com',
+    participations: 26,
+    isAdmin: false,
+  },
+  {
+    id: 6,
+    firstName: 'Nikola',
+    lastName: 'Kristanović',
+    email: 'nikola.kristanovic@gmail.com',
+    participations: 24,
+    isAdmin: false,
+  },
+  {
+    id: 7,
+    firstName: 'Tomislav',
+    lastName: 'Fulir',
+    email: 'tomislav.fulir@gmail.com',
+    participations: 26,
+    isAdmin: true,
+  },
+  {
+    id: 8,
+    firstName: 'Borna',
+    lastName: 'Grilec',
+    email: 'borna.grilec@gmail.com',
+    participations: 31,
+    isAdmin: false,
+  },
+];
